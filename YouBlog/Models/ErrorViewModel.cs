19d7450cf11b7079633f91f21889e21af6594e74
acsili <ilyaovchinnikov4@gmail.com>
2024-03-06 14:55:13 +0400

    init

---
+++ YouBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YouBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
