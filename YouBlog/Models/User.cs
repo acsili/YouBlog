c5e0b979a03e72289593067f5afec3b1bb87fa91
acsili <ilyaovchinnikov4@gmail.com>
2024-03-08 21:27:03 +0400

    Add user model and delete error view model

---
+++ YouBlog/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using YouBlog.DAL.Interfaces;
+
+namespace YouBlog.Models
+{
+    public class User : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Salt { get; set; }
+        public DateTime RegisteredAt { get; set; } = DateTime.Now;
+        public string Description { get; set; }
+    }
+}
