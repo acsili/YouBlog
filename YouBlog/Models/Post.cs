981c27213b06c9da9e1c0fa20e7cbcf7ae7da287
acsili <ilyaovchinnikov4@gmail.com>
2024-03-10 20:44:16 +0400

    Add post model

---
+++ YouBlog/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using YouBlog.DAL.Interfaces;
+
+namespace YouBlog.Models
+{
+    public class Post : IEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime UpdatedDate { get; set; } = DateTime.Now;
+        public int AuthorId { get; set; }
+    }
+}
