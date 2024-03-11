aa87557dc8fb88f61bc484188b3c9e64c3b75979
acsili <ilyaovchinnikov4@gmail.com>
2024-03-11 19:13:45 +0400

    add comment and tag models

---
+++ YouBlog/Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿using YouBlog.DAL.Interfaces;
+
+namespace YouBlog.Models
+{
+    public class Comment : IEntity
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public int AuthorId { get; set; }
+    }
+}
---
+++ YouBlog/Models/Tag.cs
@@ -0,0 +1,10 @@
+﻿using YouBlog.DAL.Interfaces;
+
+namespace YouBlog.Models
+{
+    public class Tag : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
