b361ca08bd4cff05ec76c67fbcbd9687be682808
acsili <ilyaovchinnikov4@gmail.com>
2024-04-02 19:04:20 +0400

    Update Program.cs

--- YouBlog/Program.cs
+++ YouBlog/Program.cs
@@ -1,16 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using YouBlog.DAL;
+using YouBlog.DAL.Interfaces;
+using YouBlog.DAL.Repository;
+using YouBlog.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationContext>(options
+    => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IRepository<User>, UserRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
