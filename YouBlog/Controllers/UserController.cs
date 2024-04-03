3e9faf20d45faac2cbd075deb77b4786a9a4e1e2
acsili <ilyaovchinnikov4@gmail.com>
2024-04-03 19:07:34 +0400

    Add UserController.cs

---
+++ YouBlog/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YouBlog.DAL.Interfaces;
+using YouBlog.Models;
+
+namespace YouBlog.Controllers
+{
+    public class UserController : Controller
+    {
+
+        private IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository) 
+        { 
+
+            _userRepository = userRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+
+            var response = await _userRepository.GetAll();
+
+            return View(response);
+        }
+    }
+}
